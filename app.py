fd68c7052274f678ab18eebfe1f11bdb7d107ddd
Luis Bianchin <labianchin@users.noreply.github.com>
2016-01-11 11:27:49 -0200

    Move signal handling

--- app.py
+++ app.py
@@ -9,19 +9,19 @@ port_redis=os.environ.get('PORT_REDIS', '6379')
 app = Flask(__name__)
 redis = Redis(host=host_redis, port=port_redis)
 
-def handler(signum, frame):
-    print 'Signal handler called with signal', signum
-    server.terminate()
-    server.join()
-
-signal.signal(signal.SIGTERM, handler)
-
 @app.route('/')
 def hello():
     redis.incr('hits')
     return 'Hello World! %s times.' % redis.get('hits')
 
 if __name__ == "__main__":
+    def handler(signum, frame):
+        print 'Signal handler called with signal', signum
+        server.terminate()
+        server.join()
+
+    signal.signal(signal.SIGTERM, handler)
+
     def run_server():
         app.run(host="0.0.0.0", debug=True)
 
