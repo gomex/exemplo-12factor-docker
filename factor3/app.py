19e617fe112baef112a105ccbb35a2b04c968380
gomex <gomex@riseup.net>
2016-06-28 22:52:51 -0300

    Update app.py

--- factor3/app.py
+++ factor3/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from redis import Redis
+host_run=os.environ.get('HOST_RUN', '0.0.0.0')
+debug=os.environ.get('DEBUG', 'True')
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 @app.route('/')
@@ -7,4 +9,4 @@ def hello():
    redis.incr('hits')
    return 'Hello World! %s times.' % redis.get('hits')
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", debug=True)
+   app.run(host=host_run, debug=True)
