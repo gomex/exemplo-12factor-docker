501dd1c5ca597db6e725665eb4a4b376073cdecb
Rafael Gomes <rgomes@thoughtworks.com>
2016-07-02 22:25:02 -0300

    Adicionando o btp

---
+++ factor5/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+USER="gomex"
+TIMESTAMP=$(date "+%Y.%m.%d-%H.%M")
+
+echo "Construindo a imagem ${USER}/app:${TIMESTAMP}"
+docker build -t ${USER}/app:${TIMESTAMP} .
+
+echo "Marcando a tag latest também"
+docker tag ${USER}/app:${TIMESTAMP} ${USER}/app:latest
+
+echo "Enviando a imagem para nuvem docker"
+docker push ${USER}/app:${TIMESTAMP}
+docker push ${USER}/app:latest
